08c0724c478711ffd8f0bbb8dd2acc9303d6e743
KingLauri200 <laurenz.haase@gmx.de>
2023-12-11 21:14:23 +0100

    added spring jpa

--- gettingStarted/Blockchain/src/main/java/Blockchain/Classes/Block.java
+++ gettingStarted/Blockchain/src/main/java/Blockchain/Classes/Block.java
@@ -2,6 +2,11 @@ package Blockchain.Classes;
 import java.util.Arrays;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Block {
 
     private final int prevHash;
