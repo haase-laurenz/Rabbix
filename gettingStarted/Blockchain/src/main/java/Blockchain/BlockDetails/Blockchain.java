4828ec8d74b8f867ab7e50396549bf2a742756b5
KingLauri200 <laurenz.haase@gmx.de>
2023-12-12 20:23:46 +0100

    changed block dir

--- gettingStarted/Blockchain/src/main/java/Blockchain/BlockDetails/Blockchain.java
+++ gettingStarted/Blockchain/src/main/java/Blockchain/BlockDetails/Blockchain.java
@@ -1,4 +1,4 @@
-package Blockchain.Classes;
+package Blockchain.BlockDetails;
 
 import java.util.ArrayList;
 import java.util.List;
--- gettingStarted/Blockchain/src/main/java/Blockchain/Home/HomeController.java
+++ gettingStarted/Blockchain/src/main/java/Blockchain/Home/HomeController.java
@@ -1,6 +1,16 @@
 package Blockchain.Home;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class HomeController {
     
-    
+    @GetMapping(path = "/home")
+    public String overview(Model model){
+
+        return "home";
+    }
 }
+
